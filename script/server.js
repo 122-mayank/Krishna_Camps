e751d6921a4e8b5263513aaf627846ba2bbff6a4
122-Mayank <mayanksaini20758@gmail.com>
2025-10-23 13:17:06 +0530

    Changing js and ejs files

--- script/server.js
+++ script/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
 const app = express();
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'../','views'));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 // console.log(__dirname);
 app.use(express.static(path.join(__dirname,'../','style')));
@@ -23,7 +29,58 @@ app.get('/about',(req,res,next)=>{
        res.render('about');
 });
 
+app.get('/contact-us',(req,res,next)=>{
+        res.render('contact');
+});
 
+app.post('/submit' , async(req,res)=>{
+
+       console.log(req.body);
+
+ const { schoolname, email, contact, designation } = req.body;
+       //create transporter
+       let transporter = nodemailer.createTransport({
+
+              service:'gmail',
+              auth:{
+                     user : process.env.SENDER_EMAIL,
+                     pass:process.env.APP_PASSWORD
+              }
+       });
+
+       //prepare email content
+
+        let mailOptions = {
+    from: `"Krishna Camps Website" <${process.env.SENDER_EMAIL}>`,
+    to: process.env.MANAGER_EMAIL,
+    replyTo: email, // school email
+    subject: `New Contact Form Submission from ${schoolname}`,
+    html: `
+        <h2>New Contact Form Submission</h2>
+        <p><strong>School Name:</strong> ${schoolname}</p>
+        <p><strong>Email ID:</strong> ${email}</p>
+        <p><strong>Contact Number:</strong> ${contact}</p>
+        <p><strong>Designation:</strong> ${designation}</p>
+    `
+};
+
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log('Email sent successfully!');
+       //  alert("Email sent successfully!!");
+        res.redirect('/');  // or show a success page
+    } catch(err) {
+        console.error('Error sending email:', err);
+        res.send('Something went wrong. Please try again.');
+    }
+
+
+});
+
+// app.post('/submit',(req,res,next)=>{
+//         console.log(req.body);
+//         res.render('index');
+// });
 
 const PORT = 3002;
 app.listen(PORT,()=>{
