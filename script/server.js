b25d171e4c490af0726e99ec6a4421e4377e3c04
122-Mayank <mayanksaini20758@gmail.com>
2025-10-24 22:25:16 +0530

    Changing the files

--- script/server.js
+++ script/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const nodemailer = require('nodemailer');
+const SibTransport = require('nodemailer-sendinblue-transport');
 require('dotenv').config();
 
 const app = express();
